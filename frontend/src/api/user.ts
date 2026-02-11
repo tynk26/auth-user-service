bcf9711da587ae1c8b0ef5ef735f0b6c5e5a51f8
tynk26 <tynk2609@gmail.com>
2026-02-11 13:56:24 +0900

    feat: API 호출 함수 작성, register/login 구현

---
+++ frontend/src/api/user.ts
@@ -0,0 +1,20 @@
+// 백엔드 User API 호출 함수
+const BASE_URL = "http://localhost:3000/user";
+
+export const register = async (username: string, password: string) => {
+  const res = await fetch(`${BASE_URL}/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  });
+  return res.json();
+};
+
+export const login = async (username: string, password: string) => {
+  const res = await fetch(`${BASE_URL}/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  });
+  return res.json();
+};
